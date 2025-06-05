35574825f10b0dd95de8347309c8c4604b48e6b6
rakshitha <gaderakshitha4@gmail.com>
2025-06-05 19:06:29 +0530

    Duplicate log is removed

--- src/main/java/utils/Log.java
+++ src/main/java/utils/Log.java
@@ -24,26 +24,6 @@ public class Log {
         logger.debug(message);
     }
 
-}
 
-public class Log {
-
-    private static final Logger logger = LogManager.getLogger(IdpApplication.class);
-
-    public static void info(String message) {
-        logger.info(message);
-    }
-
-    public static void warn(String message) {
-        logger.warn(message);
-    }
-
-    public static void error(String message) {
-        logger.error(message);
-    }
-
-    public static void debug(String message) {
-        logger.debug(message);
-    }
 
 }
